3f6920ee9241d76ed7f225130d7974b91cf7b082
Md Awsaf Alam <1505114.maaa@ugrad.cse.buet.ac.bd>
2023-07-16 13:19:35 +0600

    Added graphql

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,30 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        // outputAs: 'class',
+      },
+      // playground: false,
+    }),
+  ],
 })
 export class AppModule {}
